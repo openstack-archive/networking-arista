3ef0b4430b5789e38948287786d538f434ac2950
Sukhdev <sukhdev@aristanetworks.com>
2015-01-20 10:29:52 -0800

    Moving Arista ML2 driver from neutron tree
    This fix moves all the back-end drivers for of
    Arista ML2 driver from Neutron main tree to
    networking-arista project.
    
    Change-Id: I030a3aa310205da27929a405ec4619e78824fcc2
    Closes-bug: 1412577

--- networking_arista/__init__.py
+++ networking_arista/__init__.py
@@ -12,8 +12,11 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import gettext
 import pbr.version
 
 
 __version__ = pbr.version.VersionInfo(
     'networking_arista').version_string()
+
+gettext.install('networking_arista', unicode=1)
