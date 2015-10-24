5dc17388f39a605334dda599e97d204796c32a57
Shashank Hegde <hegde.shashank@gmail.com>
2015-10-23 17:24:33 -0700

    Adding database migration scripts
    The alembic migration scripts create the base version of the database tables
    used by the networking-arista package.
    
    Change-Id: Icefc9b395a3c15f7bdfde7c83545d90e53185eea

---
+++ networking_arista/db/migration/alembic_migrations/versions/liberty/contract/47036dc8697a_initial_db_version_contract.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2015 Arista Networks, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""Initial db version
+
+Revision ID: 47036dc8697a
+Create Date: 2015-10-23 14:37:49.594974
+
+"""
+
+from neutron.db.migration import cli
+
+# revision identifiers, used by Alembic.
+revision = '47036dc8697a'
+down_revision = '296b4e0236e0'
+branch_labels = (cli.CONTRACT_BRANCH,)
+
+
+def upgrade():
+    pass
