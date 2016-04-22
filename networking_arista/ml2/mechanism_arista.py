790444b719dc54114141df57904c429ddcc398a4
Sukhdev Kapur <sukhdev@aristanetworks.com>
2016-04-22 16:26:31 -0700

    Do not delete a network with ports attached to it
    If a network has ports attached to it, do not delete the network
    instead raise an exception so that user knows about this
    condition.
    
    Additionally, during initialization, when networks are deleted,
    remove all the ports associated with the network.
    
    Change-Id: Idb72305ac37dfe0c865fe09fbc5ab784d807906d
    Closes-bug: #1573207

--- networking_arista/ml2/mechanism_arista.py
+++ networking_arista/ml2/mechanism_arista.py
@@ -165,8 +165,14 @@ class AristaDriver(driver_api.MechanismDriver):
         network_id = network['id']
         tenant_id = network['tenant_id'] or INTERNAL_TENANT_ID
         with self.eos_sync_lock:
+            all_ports = db_lib.get_all_ports_for_network(network_id)
             if db_lib.is_network_provisioned(tenant_id, network_id):
-                db_lib.forget_network(tenant_id, network_id)
+                if all_ports:
+                    LOG.info(_LI('Network %s can not be deleted as it '
+                                 'has ports attached to it'), network_id)
+                    raise ml2_exc.MechanismDriverError()
+                else:
+                    db_lib.forget_network(tenant_id, network_id)
 
     def delete_network_postcommit(self, context):
         """Send network delete request to Arista HW."""
@@ -474,3 +480,4 @@ class AristaDriver(driver_api.MechanismDriver):
         for net_id in set(arista_db_nets.keys()).difference(neutron_net_ids):
             tenant_network = arista_db_nets[net_id]
             db_lib.forget_network(tenant_network['tenantId'], net_id)
+            db_lib.forget_all_ports_for_network(net_id)
