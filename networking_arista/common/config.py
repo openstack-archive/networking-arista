0945bdd2b03a0d423d87ba71e945dcbd4549ac71
Sukhdev Kapur <sukhdev@aristanetworks.com>
2016-05-26 18:28:50 -0700

    Adding Ironic Support
    This patch adds Ironic support to Arista ML2 driver.
    This patch also adds support for Security Groups for
    baremetal instances
    
    Change-Id: I7449f511d172678a77e1b5f087f1b182a78948c0

--- networking_arista/common/config.py
+++ networking_arista/common/config.py
@@ -70,7 +70,25 @@ ARISTA_DRIVER_OPTS = [
                       'with the region name registered (or known) to keystone '
                       'service. Authentication with Keysotne is performed by '
                       'EOS. This is optional. If not set, a value of '
-                      '"RegionOne" is assumed.'))
+                      '"RegionOne" is assumed.')),
+    cfg.BoolOpt('sec_group_support',
+                default=False,
+                help=_('Specifies if the Security Groups needs to deployed '
+                       'for baremetal deployments. If this flag is set to '
+                       'True, this means switch_info(see below) must be '
+                       'defined. If this flag is not defined, it is assumed '
+                       'to be False')),
+    cfg.ListOpt('switch_info',
+                default=[],
+                help=_('This is a comma separated list of Arista Switches '
+                       'where the Security groups (i.e. ACLs) need to be '
+                       'applied. Each string has three values separated  '
+                       'by : in the follow format '
+                       '<IP of switch>:<username>:<password>, ...... '
+                       'For Example: 172.13.23.55:admin:admin, '
+                       '172.13.23.56:admin:admin, .... '
+                       'This is required if sec_group_support is set to '
+                       '"True"')),
 ]
 
 
