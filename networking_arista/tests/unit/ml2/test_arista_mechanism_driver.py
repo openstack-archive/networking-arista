977e823b66e59ae5fc5c4ccfb17351228a80b979
Mitchell Jameson <mitchell@arista.com>
2016-08-09 14:59:03 -0700

    Removed cli command check in sync to fix CVX publication issue.
    Change-Id: Id2137a96ea420fbf293c70bc35e93b42a33a1e3f

--- networking_arista/tests/unit/ml2/test_arista_mechanism_driver.py
+++ networking_arista/tests/unit/ml2/test_arista_mechanism_driver.py
@@ -1088,7 +1088,6 @@ class SyncServiceTest(testlib_api.SqlTestCase):
             mock.call.get_region_updated_time(),
             mock.call.sync_start(),
             mock.call.register_with_eos(sync=True),
-            mock.call.check_cli_commands(),
             mock.call.get_tenants(),
             mock.call.create_network_bulk(
                 tenant_id,
@@ -1175,7 +1174,6 @@ class SyncServiceTest(testlib_api.SqlTestCase):
             mock.call.get_region_updated_time().__nonzero__(),
             mock.call.sync_start(),
             mock.call.register_with_eos(sync=True),
-            mock.call.check_cli_commands(),
             mock.call.get_tenants(),
             mock.call.create_network_bulk(
                 tenant_2_id,
@@ -1231,7 +1229,6 @@ class SyncServiceTest(testlib_api.SqlTestCase):
             mock.call.get_region_updated_time().__nonzero__(),
             mock.call.sync_start(),
             mock.call.register_with_eos(sync=True),
-            mock.call.check_cli_commands(),
             mock.call.get_tenants(),
             mock.call.create_network_bulk(
                 tenant_1_id,
