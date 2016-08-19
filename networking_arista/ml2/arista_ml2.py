6f2e7b2483429b8590ace6351b4914940f257c71
Doug Wiegley <dougw@a10networks.com>
2016-08-19 14:29:18 +0100

    Remove reference to neutron.i18n
    Partially-implements: bp neutron-lib
    
    Change-Id: I213c83951c7c61b2365da96da1bc8251eed9d0c2

--- networking_arista/ml2/arista_ml2.py
+++ networking_arista/ml2/arista_ml2.py
@@ -25,12 +25,10 @@ from oslo_utils import excutils
 import requests
 from six import add_metaclass
 
-from neutron._i18n import _LE
-from neutron._i18n import _LI
-from neutron._i18n import _LW
 from neutron.common import constants as n_const
 from neutron.extensions import portbindings
 
+from networking_arista._i18n import _, _LI, _LW, _LE
 from networking_arista.common import db_lib
 from networking_arista.common import exceptions as arista_exc
 from networking_arista.ml2 import arista_sec_gp
