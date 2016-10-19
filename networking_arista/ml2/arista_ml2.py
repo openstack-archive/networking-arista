4701262242804da8ff28779a3bc1b01325031885
Paul Bourke <paul.bourke@oracle.com>
2016-10-19 16:00:18 +0100

    Redact keystone password from logs in arista_ml2
    Within networking_arista/ml2/arista_ml2.py, the register_with_eos()
    method takes care to redact the keystone password by maintaining a copy
    of "cmds" in "log_cmds".
    
    cmds and log_cmds are then passed through _run_openstack_cmds() ->
    _run_eos_cmds() -> _send_eapi_req(). However, _send_eapi_req() does not
    have an argument for log_cmds, and ends up logging the password on line
    1118.
    
    Fix this by updating _send_eapi_req() to accept 'commands_to_log', and
    subbing those into the msg to be logged.
    
    Change-Id: I5f6eea1d384fa07bf88fcac201316779abdc85b1
    Closes-Bug: #1634937

--- networking_arista/ml2/arista_ml2.py
+++ networking_arista/ml2/arista_ml2.py
@@ -15,6 +15,7 @@
 
 import abc
 import base64
+import copy
 import json
 import os
 import socket
@@ -1085,7 +1086,7 @@ class AristaRPCWrapperEapi(AristaRPCWrapperBase):
             'resource-pool': []
         }
 
-    def _send_eapi_req(self, cmds):
+    def _send_eapi_req(self, cmds, commands_to_log=None):
         # This method handles all EAPI requests (using the requests library)
         # and returns either None or response.json()['result'] from the EAPI
         # request.
@@ -1113,8 +1114,10 @@ class AristaRPCWrapperEapi(AristaRPCWrapperBase):
         response = None
 
         try:
+            log_data = copy.deepcopy(data)
+            log_data['params']['cmds'] = commands_to_log
             msg = (_('EAPI request to %(ip)s contains %(cmd)s') %
-                   {'ip': self._server_ip, 'cmd': json.dumps(data)})
+                   {'ip': self._server_ip, 'cmd': json.dumps(log_data)})
             LOG.info(msg)
             response = requests.post(url, timeout=self.conn_timeout,
                                      verify=False, data=json.dumps(data))
@@ -1693,7 +1696,8 @@ class AristaRPCWrapperEapi(AristaRPCWrapperBase):
         # this returns array of return values for every command in
         # full_command list
         try:
-            response = self._send_eapi_req(cmds=commands)
+            response = self._send_eapi_req(cmds=commands,
+                                           commands_to_log=log_cmds)
             if response is None:
                 # Reset the server as we failed communicating with it
                 self._server_ip = None
