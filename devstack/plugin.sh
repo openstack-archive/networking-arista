4cdd2941c9480ea4ffff7f106ae08400dfb253d6
Shashank Hegde <shashank@arista.com>
2016-11-30 11:24:31 -0800

    Adding all config options to devstack plugin
    Change-Id: I06615b6d69150de38fb55cea774882af0c51b52b

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -3,7 +3,7 @@
 function install_lldp() {
     echo_summary "Installing LLDP"
     install_package lldpd
-    start_service lldpd
+    restart_service lldpd
 }
 
 function install_arista_driver() {
@@ -19,7 +19,53 @@ function configure_arista() {
     iniset $ARISTA_ML2_CONF_FILE ml2_arista eapi_username $ARISTA_EAPI_USERNAME
     iniset $ARISTA_ML2_CONF_FILE ml2_arista eapi_password $ARISTA_EAPI_PASSWORD
     iniset $ARISTA_ML2_CONF_FILE ml2_arista api_type $ARISTA_API_TYPE
-    iniset $ARISTA_ML2_CONF_FILE ml2_arista region_name $REGION_NAME
+    iniset $ARISTA_ML2_CONF_FILE ml2_arista region_name $ARISTA_REGION_NAME
+
+    if [ ${ARISTA_USE_FQDN+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE ml2_arista use_fqdn $ARISTA_USE_FQDN
+    fi
+
+    if [ ${ARISTA_ML2_SYNC_INTERVAL+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE ml2_arista sync_interval $ARISTA_ML2_SYNC_INTERVAL
+    fi
+    if [ ${ARISTA_SEC_GROUP_SUPPORT+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE ml2_arista sec_group_support $ARISTA_SEC_GROUP_SUPPORT
+    fi
+    if [ ${ARISTA_SWITCH_INFO+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE ml2_arista switch_info $ARISTA_SWITCH_INFO
+    fi
+
+    if [ ${ARISTA_PRIMARY_L3_HOST+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE l3_arista primary_l3_host $ARISTA_PRIMARY_L3_HOST
+    fi
+    if [ ${ARISTA_PRIMARY_L3_HOST_USERNAME+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE l3_arista primary_l3_host_username $ARISTA_PRIMARY_L3_HOST_USERNAME
+    fi
+    if [ ${ARISTA_PRIMARY_L3_HOST_PASSWORD+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE l3_arista primary_l3_host_password $ARISTA_PRIMARY_L3_HOST_PASSWORD
+    fi
+    if [ ${ARISTA_SECONDARY_L3_HOST+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE l3_arista secondary_l3_host $ARISTA_SECONDARY_L3_HOST
+    fi
+    if [ ${ARISTA_SECONDARY_L3_HOST_USERNAME+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE l3_arista secondary_l3_host_username $ARISTA_SECONDARY_L3_HOST_USERNAME
+    fi
+    if [ ${ARISTA_SECONDARY_L3_HOST_PASSWORD+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE l3_arista secondary_l3_host_password $ARISTA_SECONDARY_L3_HOST_PASSWORD
+    fi
+    if [ ${ARISTA_MLAG_CONFIG+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE l3_arista mlag_config $ARISTA_MLAG_CONFIG
+    fi
+    if [ ${ARISTA_USE_VRF+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE l3_arista use_vrf $ARISTA_USE_VRF
+    fi
+    if [ ${ARISTA_L3_SYNC_INTERVAL+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE l3_arista l3_sync_interval $ARISTA_L3_SYNC_INTERVAL
+    fi
+
+    if [ ${ARISTA_TYPE_DRIVER_SYNC_INTERVAL+x} ]; then
+        iniset $ARISTA_ML2_CONF_FILE arista_type_driver sync_interval $ARISTA_TYPE_DRIVER_SYNC_INTERVAL
+    fi
 }
 
 if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
