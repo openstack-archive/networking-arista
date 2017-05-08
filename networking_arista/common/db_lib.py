b0ff72c2a8951f43044b19bd969cf0f19a24d707
Mitchell Jameson <mitchell@arista.com>
2017-05-07 17:15:02 -0700

    Use vnic_type to determine whether a port is baremetal
    In ocata, the tenant network port for baremetal instances
    is owned by compute:<zone> instead of baremetal:<zone>. The
    vnic_type for those ports is still 'baremetal', so use the
    vnic_type to identify baremetal ports.
    
    Change-Id: I677148a7964ea4b3673cc918999073b2cd5caafd

--- networking_arista/common/db_lib.py
+++ networking_arista/common/db_lib.py
@@ -416,7 +416,7 @@ def get_tenants():
         return res
 
 
-def _make_bm_port_dict(record):
+def _make_port_dict(record):
     """Make a dict from the BM profile DB record."""
     return {'port_id': record.port_id,
             'host_id': record.host,
@@ -431,8 +431,19 @@ def get_all_baremetal_ports():
         querry = session.query(ml2_models.PortBinding)
         bm_ports = querry.filter_by(vnic_type='baremetal').all()
 
-        return {bm_port.port_id: _make_bm_port_dict(bm_port)
+        return {bm_port.port_id: _make_port_dict(bm_port)
                 for bm_port in bm_ports}
+
+
+def get_all_portbindings():
+    """Returns a list of all ports bindings."""
+    session = db.get_session()
+    with session.begin():
+        query = session.query(ml2_models.PortBinding)
+        ports = query.all()
+
+        return {port.port_id: _make_port_dict(port)
+                for port in ports}
 
 
 def get_port_binding_level(filters):
