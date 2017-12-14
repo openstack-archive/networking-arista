4a6fecbec4e248dcc688238700b40c59f69406c3
Mitchell Jameson <mitchell@arista.com>
2017-12-14 15:07:36 -0800

    Cleanup synchronization
    Tailor sync logic to the json_api. Since GET/DELETE/POST logic
    is so similar for all resource types, a great deal of common
    code can be used. Each resource must simply define a neutron db
    get helper, a cvx endpoint and a conversion helper from neutron
    format to CVX format.
    
    Change-Id: I91273d3da3ddbcee970ab10dbb51d726a417fae9

--- networking_arista/tests/unit/ml2/test_arista_sync.py
+++ networking_arista/tests/unit/ml2/test_arista_sync.py
@@ -65,45 +65,12 @@ class SyncServiceTest(testlib_api.SqlTestCase):
             'regionTimestamp': '0',
         }
 
-        tenant_id = 'tenant-1'
-        network_id = 'aaaaaaaa-1111-1111-1111-aaaaaaaaaaaa'
-        segmentation_id = 42
-        n_ctx = utils.create_network(tenant_id, network_id, segmentation_id)
-
-        self.rpc.get_tenants.return_value = {}
-
         self.rpc.sync_start.return_value = True
         self.rpc.sync_end.return_value = True
         self.rpc.check_cvx_availability.return_value = True
-
-        self.rpc._baremetal_supported.return_value = False
-        self.rpc.get_all_baremetal_hosts.return_value = {}
-
+        self.sync_service.synchronize = mock.MagicMock()
         self.sync_service.do_synchronize()
-
-        expected_calls = [
-            mock.call.perform_sync_of_sg(),
-            mock.call.check_cvx_availability(),
-            mock.call.get_region_updated_time(),
-            mock.call.sync_start(),
-            mock.call.register_with_eos(sync=True),
-            mock.call.get_tenants(),
-            mock.call.create_network_bulk(
-                tenant_id,
-                [{'network_id': network_id,
-                  'segments': n_ctx.network_segments,
-                  'network_name': network_id,
-                  'shared': False}],
-                sync=True),
-            mock.call.sync_end(),
-            mock.call.get_region_updated_time()
-        ]
-        self.assertTrue(self.rpc.mock_calls == expected_calls,
-                        "Seen: %s\nExpected: %s" % (
-                            self.rpc.mock_calls,
-                            expected_calls,
-                            )
-                        )
+        self.sync_service.synchronize.assert_called_once()
 
     def test_sync_start_failure(self):
         """Tests that we force another sync when sync_start fails.
@@ -157,235 +124,29 @@ class SyncServiceTest(testlib_api.SqlTestCase):
                             )
                         )
 
-    def test_synchronize_one_network(self):
-        """Test to ensure that only the required resources are sent to EOS."""
 
-        # Store two tenants in a db and a single tenant in EOS.
-        # The sync should send details of the second tenant to EOS
-        tenant_1_id = 'tenant-1'
-        tenant_1_net_1_id = 'aaaaaaaa-1111-1111-1111-aaaaaaaaaaaa'
-        tenant_1_net_1_seg_id = 11
-        utils.create_network(tenant_1_id, tenant_1_net_1_id,
-                             tenant_1_net_1_seg_id)
+class SynchronizeTest(testlib_api.SqlTestCase):
 
-        tenant_2_id = 'tenant-2'
-        tenant_2_net_1_id = 'bbbbbbbb-1111-1111-1111-bbbbbbbbbbbb'
-        tenant_2_net_1_seg_id = 21
-        n2_ctx = utils.create_network(tenant_2_id, tenant_2_net_1_id,
-                                      tenant_2_net_1_seg_id)
+    def setUp(self):
+        super(SynchronizeTest, self).setUp()
+        plugin_klass = importutils.import_class(
+            "neutron.db.db_base_plugin_v2.NeutronDbPluginV2")
+        directory.add_plugin(plugin_constants.CORE, plugin_klass())
+        utils.setup_scenario()
+        self.rpc = utils.MockCvx('region')
+        ndb = db_lib.NeutronNets()
+        self.sync_service = arista_sync.SyncService(self.rpc, ndb)
 
-        self.rpc.get_tenants.return_value = {
-            tenant_1_id: {
-                'tenantVmInstances': {},
-                'tenantBaremetalInstances': {},
-                'tenantNetworks': {
-                    tenant_1_net_1_id: {
-                        'networkId': tenant_1_net_1_id,
-                        'shared': False,
-                        'networkName': 'Net1',
-                        'segmenationType': 'vlan',
-                        'segmentationTypeId': tenant_1_net_1_seg_id,
-                    }
-                }
-            }
-        }
+    def test_full_sync_api_calls(self):
+        for i in range(len(self.sync_service.sync_order)):
+            self.sync_service.sync_order[i] = mock.MagicMock()
+        self.sync_service.synchronize()
+        for resource_type in self.sync_service.sync_order:
+            resource_type.clear_all_data.assert_called_once()
+            resource_type.delete_cvx_resources.assert_called_once()
+            resource_type.create_cvx_resources.assert_called_once()
 
-        self.rpc.sync_start.return_value = True
-        self.rpc.sync_end.return_value = True
-        self.rpc.check_cvx_availability.return_value = True
-        self.rpc.get_region_updated_time.return_value = {'regionTimestamp': 1}
-
-        self.rpc._baremetal_supported.return_value = False
-        self.rpc.get_all_baremetal_hosts.return_value = {}
-
-        self.sync_service.do_synchronize()
-
-        expected_calls = [
-            mock.call.perform_sync_of_sg(),
-            mock.call.check_cvx_availability(),
-            mock.call.get_region_updated_time(),
-            mock.call.sync_start(),
-            mock.call.register_with_eos(sync=True),
-            mock.call.get_tenants(),
-
-            mock.call.create_network_bulk(
-                tenant_2_id,
-                [{'network_id': tenant_2_net_1_id,
-                  'segments': n2_ctx.network_segments,
-                  'network_name': tenant_2_net_1_id,
-                  'shared': False}],
-                sync=True),
-
-            mock.call.sync_end(),
-            mock.call.get_region_updated_time()
-        ]
-
-        self.rpc.assert_has_calls(expected_calls)
-
-    def test_synchronize_all_networks(self):
-        """Test to ensure that only the required resources are sent to EOS."""
-
-        # Store two tenants in a db and none on EOS.
-        # The sync should send details of all tenants to EOS
-        tenant_1_id = 'tenant-1'
-        tenant_1_net_1_id = 'aaaaaaaa-1111-1111-1111-aaaaaaaaaaaa'
-        tenant_1_net_1_seg_id = 11
-        n1_ctx = utils.create_network(tenant_1_id, tenant_1_net_1_id,
-                                      tenant_1_net_1_seg_id)
-
-        tenant_2_id = 'tenant-2'
-        tenant_2_net_1_id = 'bbbbbbbb-1111-1111-1111-bbbbbbbbbbbb'
-        tenant_2_net_1_seg_id = 21
-        n2_ctx = utils.create_network(tenant_2_id, tenant_2_net_1_id,
-                                      tenant_2_net_1_seg_id)
-
-        self.rpc.get_tenants.return_value = {}
-
-        self.rpc.sync_start.return_value = True
-        self.rpc.sync_end.return_value = True
-        self.rpc.check_cvx_availability.return_value = True
-        self.rpc.get_region_updated_time.return_value = {'regionTimestamp': 1}
-
-        self.rpc._baremetal_supported.return_value = False
-        self.rpc.get_all_baremetal_hosts.return_value = {}
-
-        self.sync_service.do_synchronize()
-
-        expected_calls = [
-            mock.call.perform_sync_of_sg(),
-            mock.call.check_cvx_availability(),
-            mock.call.get_region_updated_time(),
-            mock.call.sync_start(),
-            mock.call.register_with_eos(sync=True),
-            mock.call.get_tenants(),
-
-            mock.call.create_network_bulk(
-                tenant_1_id,
-                [{'network_id': tenant_1_net_1_id,
-                  'segments': n1_ctx.network_segments,
-                  'network_name': tenant_1_net_1_id,
-                  'shared': False}],
-                sync=True),
-
-            mock.call.create_network_bulk(
-                tenant_2_id,
-                [{'network_id': tenant_2_net_1_id,
-                  'segments': n2_ctx.network_segments,
-                  'network_name': tenant_2_net_1_id,
-                  'shared': False}],
-                sync=True),
-            mock.call.sync_end(),
-            mock.call.get_region_updated_time()
-        ]
-
-        # The create_instance_bulk() can be called in different order. So split
-        # it up. The first part checks if the initial set of methods are
-        # invoked.
-        idx = expected_calls.index(mock.call.get_tenants()) + 1
-        self.rpc.assert_has_calls(expected_calls[:idx])
-
-        # Check if tenant ports are created.
-        self.rpc.assert_has_calls(expected_calls[idx:idx + 2], any_order=True)
-
-        # Check if the sync end methods are invoked.
-        self.rpc.assert_has_calls(expected_calls[idx + 2:])
-
-    def test_synchronize_shared_network_ports(self):
-        """Test to ensure that shared network ports are synchronized.
-
-        This is to ensure that ports whose tenant id does not match the
-        network tenant id are still sync'd. The test stores a network and
-        2 ports in the neutron db and only the network in EOS.
-        The sync should send details of the ports to EOS.
-        """
-
-        tenant_1_id = 'tenant-1'
-        network_id = 'aaaaaaaa-1111-1111-1111-aaaaaaaaaaaa'
-        seg_id = 11
-        network_ctx = utils.create_network(tenant_1_id, network_id, seg_id,
-                                           shared=True)
-
-        port_1_id = 'port-1'
-        device_1_id = 'vm-1'
-        utils.create_port(tenant_1_id, network_id, device_1_id, port_1_id,
-                          network_ctx)
-
-        tenant_2_id = 'tenant-2'
-        port_2_id = 'port-2'
-        device_2_id = 'vm-2'
-        utils.create_port(tenant_2_id, network_id, device_2_id, port_2_id,
-                          network_ctx)
-
-        self.rpc.get_tenants.return_value = {
-            tenant_1_id: {
-                'tenantVmInstances': {},
-                'tenantBaremetalInstances': {},
-                'tenantNetworks': {
-                    network_id: {
-                        'networkId': network_id,
-                        'shared': True,
-                        'networkName': '',
-                        'segmenationType': 'vlan',
-                        'segmentationTypeId': seg_id,
-                    }
-                }
-            }
-        }
-
-        self.rpc.sync_start.return_value = True
-        self.rpc.sync_end.return_value = True
-        self.rpc.check_cvx_availability.return_value = True
-        self.rpc.get_region_updated_time.return_value = {'regionTimestamp': 1}
-
-        self.rpc._baremetal_supported.return_value = False
-        self.rpc.get_all_baremetal_hosts.return_value = {}
-
-        self.sync_service.do_synchronize()
-
-        expected_calls = [
-            mock.call.perform_sync_of_sg(),
-            mock.call.check_cvx_availability(),
-            mock.call.get_region_updated_time(),
-            mock.call.sync_start(),
-            mock.call.register_with_eos(sync=True),
-            mock.call.get_tenants(),
-
-            mock.call.create_instance_bulk(
-                tenant_1_id,
-                {port_1_id: {'device_owner': 'compute',
-                             'device_id': device_1_id,
-                             'name': '',
-                             'id': port_1_id,
-                             'tenant_id': tenant_1_id,
-                             'network_id': network_id}},
-                db_lib.get_instance_ports(tenant_1_id),
-                {},
-                sync=True),
-
-            mock.call.create_instance_bulk(
-                tenant_2_id,
-                {port_2_id: {'device_owner': 'compute',
-                             'device_id': device_2_id,
-                             'name': '',
-                             'id': port_2_id,
-                             'tenant_id': tenant_2_id,
-                             'network_id': network_id}},
-                db_lib.get_instance_ports(tenant_2_id),
-                {},
-                sync=True),
-
-            mock.call.sync_end(),
-            mock.call.get_region_updated_time()
-        ]
-
-        # The create_instance_bulk() can be called in different order. So split
-        # it up. The first part checks if the initial set of methods are
-        # invoked.
-        idx = expected_calls.index(mock.call.get_tenants()) + 1
-        self.rpc.assert_has_calls(expected_calls[:idx])
-
-        # Check if tenant ports are created.
-        self.rpc.assert_has_calls(expected_calls[idx:idx + 2], any_order=True)
-
-        # Check if the sync end methods are invoked.
-        self.rpc.assert_has_calls(expected_calls[idx + 2:])
+    def test_full_sync_cvx_populated(self):
+        self.sync_service.synchronize()
+        for endpoint, data in self.rpc.endpoint_data.items():
+            self.assertNotEqual(data, {})
