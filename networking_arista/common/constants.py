a5d29afae80651d346db9c202b00aec84946ad1a
Mitchell Jameson <mitchell@arista.com>
2018-04-18 16:11:49 -0700

    Cleanup Arista VLAN Type Driver
    Remove ndb usage from the VLAN type driver, clarify naming.
    
    (I also slipped in some cleanup of unused classes and constants.)
    
    Change-Id: Icd0623bde15d6357e5624776367f055dae1da339

--- networking_arista/common/constants.py
+++ networking_arista/common/constants.py
@@ -53,6 +53,4 @@ MECHANISM_DRV_NAME = 'arista'
 
 # EAPI error messages of interest
 EOS_UNREACHABLE_MSG = _('Unable to reach EOS')
-UNABLE_TO_DELETE_PORT_MSG = _('Unable to delete port from EOS')
-UNABLE_TO_DELETE_DEVICE_MSG = _('Unable to delete device')
 ERR_CVX_NOT_LEADER = _('Only available on cluster leader')
