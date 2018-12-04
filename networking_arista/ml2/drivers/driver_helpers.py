06cec5e5862b500145a565c97287799eb70a4d55
Mitchell Jameson <mitchell@arista.com>
2018-12-03 17:37:17 -0800

    Allow arista_vlan type driver to startup when CVX is not reachable
    When the arista_vlan type driver is enabled, but CVX is not reachable,
    Neutron cannot be started as the parsing of available VLANs hits an
    exception. This change causes available VLANs to be an empty list
    at startup, which will prevent VLAN network creation until CVX is reachable,
    but will not interfere with Neutron startup.
    
    Change-Id: I100a62da7782023e9acb167662c2a5159bedf338
    (cherry picked from commit 21cd790be4f703c764da36389b50d8b97286ec31)

--- networking_arista/ml2/drivers/driver_helpers.py
+++ networking_arista/ml2/drivers/driver_helpers.py
@@ -98,6 +98,7 @@ class VlanSyncService(object):
         LOG.info(_LI('Syncing VLANs with EOS'))
         try:
             self._rpc.register_with_eos()
+            self._rpc.check_vlan_type_driver_commands()
             vlan_pool = self._rpc.get_vlan_allocation()
         except arista_exc.AristaRpcError:
             LOG.warning(EOS_UNREACHABLE_MSG)
